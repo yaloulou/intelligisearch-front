c86463a55c5147dcf90ba7c38cb5b336e9dee25b
Lucien Bangelesa <ir.bangelesa@gmail.com>
2026-09-23 17:35:14 +0100

    Add entities description

--- src/pages/Dashboard/ProfileDetails.vue
+++ src/pages/Dashboard/ProfileDetails.vue
@@ -17,6 +17,17 @@
             <v-col cols="12" md="8">
               <h2 class="mb-2">{{ entity.name || "Sans nom" }}</h2>
 
+<!-- Description de l'entité -->
+              <div v-if="entity.description" class="mb-3">
+                <div class="text-subtitle-2 grey--text mb-1">
+                  <v-icon small left>mdi-text-box-outline</v-icon>
+                  Description
+                </div>
+                <p class="body-1" style="white-space: pre-line;">
+                  {{ entity.description }}
+                </p>
+              </div>
+
               <v-divider class="my-3"></v-divider>
 
               <template v-if="isOrganization">
